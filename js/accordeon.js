ba04e87ab44b3bc0859e910ffd85af15a526513a
EmilieLarocque <effetxmer@gmail.com>
2026-07-06 12:00:10 -0400

    modfication pour l'utiliser sur 2 colonnes

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,27 +1,3 @@
-const accordionElement = document.getElementById('accordion-example');
-
-// create an array of objects with the id, trigger element (eg. button), and the content element
-const accordionItems = [
-    {
-        id: 'accordion-example-heading-1',
-        triggerEl: document.querySelector('#accordion-example-heading-1'),
-        targetEl: document.querySelector('#accordion-example-body-1'),
-        active: true
-    },
-    {
-        id: 'accordion-example-heading-2',
-        triggerEl: document.querySelector('#accordion-example-heading-2'),
-        targetEl: document.querySelector('#accordion-example-body-2'),
-        active: false
-    },
-    {
-        id: 'accordion-example-heading-3',
-        triggerEl: document.querySelector('#accordion-example-heading-3'),
-        targetEl: document.querySelector('#accordion-example-body-3'),
-        active: false
-    }
-];
-
 // options with default values
 const options = {
     alwaysOpen: true,
@@ -41,8 +17,56 @@ const options = {
     },
 };
 
-// instance options object
-const instanceOptions = {
-    id: 'accordion-example',
-    override: true
-};
+// Colonne 1
+const accordion1 = new Accordion(
+    document.getElementById('accordion-col-1'),
+    [
+        {
+            id: 'accordion-example-heading-1',
+            triggerEl: document.querySelector('#accordion-example-heading-1'),
+            targetEl: document.querySelector('#accordion-example-body-1'),
+            active: false
+        },
+        {
+            id: 'accordion-example-heading-2',
+            triggerEl: document.querySelector('#accordion-example-heading-2'),
+            targetEl: document.querySelector('#accordion-example-body-2'),
+            active: false
+        },
+        {
+            id: 'accordion-example-heading-3',
+            triggerEl: document.querySelector('#accordion-example-heading-3'),
+            targetEl: document.querySelector('#accordion-example-body-3'),
+            active: false
+        }
+    ],
+    options,
+    { id: 'accordion-col-1', override: true }
+);
+
+// Colonne 2
+const accordion2 = new Accordion(
+    document.getElementById('accordion-col-2'),
+    [
+        {
+            id: 'accordion-example-heading-4',
+            triggerEl: document.querySelector('#accordion-example-heading-4'),
+            targetEl: document.querySelector('#accordion-example-body-4'),
+            active: false
+        },
+        {
+            id: 'accordion-example-heading-5',
+            triggerEl: document.querySelector('#accordion-example-heading-5'),
+            targetEl: document.querySelector('#accordion-example-body-5'),
+            active: false
+        },
+        {
+            id: 'accordion-example-heading-6',
+            triggerEl: document.querySelector('#accordion-example-heading-6'),
+            targetEl: document.querySelector('#accordion-example-body-6'),
+            active: false
+        }
+    ],
+    options,
+    { id: 'accordion-col-2', override: true }
+);
